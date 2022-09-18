70f59191a0f1c8a8143acdd3144871e41dc36fa5
HamInGyu <hfire1207@gmail.com>
2022-09-18 20:44:34 +0900

    홈 슬라이드 데이터 불러와서 구현완료

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -7,6 +7,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 const Wrapper = styled.div`
   background: black;
+  padding-bottom: 200px;
 `;
 
 const Loader = styled.div`
@@ -44,30 +45,34 @@ const Slider = styled.div`
 
 const Row = styled(motion.div)`
   display: grid;
-  gap: 10px;
+  gap: 5px;
   grid-template-columns: repeat(6, 1fr);
   width: 100%;
   position: absolute;
 `;
 
-const Box = styled(motion.div)`
+const Box = styled(motion.div)<{ bgPhoto: string }>`
   background-color: white;
+  background-image: url(${(props) => props.bgPhoto});
+  background-size: cover;
+  background-position: center center;
   height: 200px;
-  color: red;
   font-size: 66px;
 `;
 
 const rowVariants = {
   hidden: {
-    x: window.outerWidth + 10,
+    x: window.outerWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.outerWidth - 10,
+    x: -window.outerWidth - 5,
   },
 };
+
+const OFFSET = 6; //ROW 하나에 몇개의 박스가 들어갈지
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>(
@@ -75,7 +80,17 @@ function Home() {
     getMovies
   );
   const [index, setIndex] = useState(0);
-  const incraseIndex = () => setIndex((prev) => prev + 1);
+  const [leaving, setLeaving] = useState(false); //현재 다음 스크롤로 넘어가는 이벤트가 진행중인지 알려주는 state
+  const incraseIndex = () => {
+    if (data) {
+      if (leaving) return;
+      toggleLeaving();
+      const totalMovies = data.results.length - 1; //슬라이드에서 표현할 총 영화 수 (0번째 인덱스는 배너에 쓸거기 때문에 총 개수에서 뺀다.)
+      const maxIndex = Math.floor(totalMovies / OFFSET) - 1; //최대 Index 구하기, 반내림하여 모든 슬라이드가 비지 않게 했다.
+      setIndex((prev) => (prev === maxIndex ? 0 : prev + 1)); //최대 인덱스면 다음으로 넘겼을 때 제일 처음으로 돌아오게
+    }
+  };
+  const toggleLeaving = () => setLeaving((prev) => !prev); //애니메이션 진행중에 increaseIndex 막기 위해서
   return (
     <Wrapper>
       {isLoading ? (
@@ -90,7 +105,9 @@ function Home() {
             <Overview>{data?.results[0].overview}</Overview>
           </Banner>
           <Slider>
-            <AnimatePresence>
+            {/* initial = {false} -> 맨 처음에 렌더링될때도 애니메이션 적용되는거 막기 위해
+            onExitComplete={toggleLeaving} -> 애니메이션 진행되는동안 다음 스크롤 땡겨오는 이벤트 막기 위해(onExitComplete는 exit 애니메이션 끝나고 실행하는 함수) */}
+            <AnimatePresence initial={false} onExitComplete={toggleLeaving}>
               <Row
                 variants={rowVariants}
                 initial="hidden"
@@ -99,9 +116,19 @@ function Home() {
                 transition={{ type: "tween", duration: 1 }}
                 key={index}
               >
-                {[1, 2, 3, 4, 5, 6].map((i) => (
-                  <Box key={i}>{i}</Box>
-                ))}
+                {data?.results
+                  .slice(1) //0번째 인덱스 제외하고 그 다음부터 (0번째는 배너에 사용)
+                  .slice(OFFSET * index, OFFSET * index + OFFSET) //현재 인덱스에 맞는 6개만 가져오기
+                  .map(
+                    (
+                      movie //가져온 6개로 렌더링하기
+                    ) => (
+                      <Box
+                        key={movie.id}
+                        bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
+                      />
+                    )
+                  )}
               </Row>
             </AnimatePresence>
           </Slider>
