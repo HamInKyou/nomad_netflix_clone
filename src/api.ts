2c3eb1353b4f5731943d6e27bc3db6c04259a255
HamInGyu <hfire1207@gmail.com>
2022-09-18 18:46:17 +0900

    영화목록 fetch하는 api 콜링함수 생성, useQuery써서 데이터 fetch하기

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const API_KEY = "9b51164e719afc8e35930fd6b08e09c9";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (response) => response.json()
+  );
+}
