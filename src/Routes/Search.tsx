19f133208282ac001df185cd64f61b5afc670e7d
HamInGyu <hfire1207@gmail.com>
2022-09-19 20:18:52 +0900

    검색결과페이지에서 키워드 가져오기

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,4 +1,10 @@
+import { useLocation } from "react-router";
+
 function Search() {
+  const location = useLocation();
+  //URLSearchParams는 자바스크립트 내장 객체!
+  const keyword = new URLSearchParams(location.search).get("keyword");
+  console.log(keyword);
   return null;
 }
 export default Search;
